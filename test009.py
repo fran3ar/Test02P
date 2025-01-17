28f7ddbaf6595c4d19ac40e3cccb9e6d012fe9d6
fran3ar <fran3ar@gmail.com>
2025-01-17 14:03:22 -0300

    Create test009.py

---
+++ test009.py
@@ -0,0 +1,69 @@
+with st.sidebar:
+    st.header(f"Add your documents!")
+    
+    uploaded_file = st.file_uploader("Choose your `.pdf` file", type="pdf")
+
+    if uploaded_file:
+        try:
+            with tempfile.TemporaryDirectory() as temp_dir:
+                file_path = os.path.join(temp_dir, uploaded_file.name)
+                
+                with open(file_path, "wb") as f:
+                    f.write(uploaded_file.getvalue())
+                
+                file_key = f"{session_id}-{uploaded_file.name}"
+                st.write("Indexing your document...")
+
+                if file_key not in st.session_state.get('file_cache', {}):
+
+                    if os.path.exists(temp_dir):
+                            loader = SimpleDirectoryReader(
+                                input_dir = temp_dir,
+                                required_exts=[".pdf"],
+                                recursive=True
+                            )
+                    else:    
+                        st.error('Could not find the file you uploaded, please check again...')
+                        st.stop()
+                    
+                    docs = loader.load_data()
+
+                    # setup llm & embedding model
+                    llm=load_llm()
+                    print("Loading embedding model...")
+                    embed_model = HuggingFaceEmbedding(model_name="nomic-ai/modernbert-embed-base", trust_remote_code=True, cache_folder='./hf_cache')
+                    print("Embedding model loaded!")
+                    # Creating an index over loaded data
+                    Settings.embed_model = embed_model
+                    index = VectorStoreIndex.from_documents(docs, show_progress=True)
+
+                    # Create the query engine, where we use a cohere reranker on the fetched nodes
+                    Settings.llm = llm
+                    query_engine = index.as_query_engine(streaming=True)
+
+                    # ====== Customise prompt template ======
+                    qa_prompt_tmpl_str = (
+                    "Context information is below.\n"
+                    "---------------------\n"
+                    "{context_str}\n"
+                    "---------------------\n"
+                    "Given the context information above I want you to think step by step to answer the query in a crisp manner, incase case you don't know the answer say 'I don't know!'.\n"
+                    "Query: {query_str}\n"
+                    "Answer: "
+                    )
+                    qa_prompt_tmpl = PromptTemplate(qa_prompt_tmpl_str)
+
+                    query_engine.update_prompts(
+                        {"response_synthesizer:text_qa_template": qa_prompt_tmpl}
+                    )
+                    
+                    st.session_state.file_cache[file_key] = query_engine
+                else:
+                    query_engine = st.session_state.file_cache[file_key]
+
+                # Inform the user that the file is processed and Display the PDF uploaded
+                st.success("Ready to Chat!")
+                display_pdf(uploaded_file)
+        except Exception as e:
+            st.error(f"An error occurred: {e}")
+            st.stop()     
